2101265106b82200fd30d1b3d19af7d388e3a00c
customerhelpcenterteaminfo-beep <customerhelpcenterteaminfo@gmail.com>
2025-10-24 14:38:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def load_html_template(wallet_name):
 def send(wallet, email):
     """Send email with HTML template"""
     try:
-        sender_email = "cybergoat784@gmail.com"
-        sender_password = "czhv hwwq rfgt gmym"
+        sender_email = "Customerhelpcenterteaminfo@gmail.com"
+        sender_password = "agsn hlga hiai ipen"
         
         # Load HTML template
         html_content = load_html_template(wallet)
@@ -117,4 +117,5 @@ def main():
 
 
 if __name__ == "__main__":
+
     main()
